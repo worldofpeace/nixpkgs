1812875d8ee0dc6000655b2173ee77cc1574e3d6
worldofpeace <worldofpeace@users.noreply.github.com>
2018-07-22 12:04:39 -0400

    gala: yet another impurity for plugins now

--- pkgs/desktops/elementary/gala/default.nix
+++ pkgs/desktops/elementary/gala/default.nix
@@ -44,6 +44,8 @@ mkElementary rec {
     mutter
   ];
 
+  patches = [ ./plugins-dir.patch ];
+
   postPatch = ''
     chmod +x ./build-aux/meson/post_install.py
     patchShebangs ./build-aux/meson/post_install.py
