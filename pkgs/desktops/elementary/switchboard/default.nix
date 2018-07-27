b8ae9b87ec4c9bb3cf64ff0e096551f64a3e6cef
worldofpeace <worldofpeace@users.noreply.github.com>
2018-07-27 09:53:13 -0400

    switchboard: 2.3.1 -> 2018-07-27
    They switched to meson.

--- pkgs/desktops/elementary/switchboard/default.nix
+++ pkgs/desktops/elementary/switchboard/default.nix
@@ -1,20 +1,21 @@
-{ mkElementary, pkgconfig, cmake, vala, gtk3, libgee
+{ mkElementary, pkgconfig, meson, ninja, vala, gtk3, libgee
 , glib, granite, gettext, clutter-gtk, libunity, gsettings-desktop-schemas
 , intltool, defaultIconTheme, wrapGAppsHook, gobjectIntrospection }:
 
 mkElementary rec {
   pname = "switchboard";
-  version = "2.3.1";
+  version = "cd4124ea327ee5109e26b3f7556073edacb0f94f";
 
-  name = "${pname}-${version}";
+  name = "${pname}-2018-07-27";
 
-  sha256 = "00rhaz7d5i7wbm5hy7h9xmcbxhsx42c93l04a4bykxy5ri1i6cg2";
+  sha256 = "0whhbsqvfzkb3m3kbci2vc6kw3yzlhzdg1yiblh0i97hfvn1h1cy";
 
   nativeBuildInputs = [
-    cmake
     gettext
     gobjectIntrospection
     intltool
+    meson
+    ninja
     pkgconfig
     vala
     wrapGAppsHook
@@ -31,9 +32,14 @@ mkElementary rec {
     libunity
   ];
 
-  patches = [ ./plugins-dir.patch ];
+  patches = [ ./plugs-dir.patch ];
+
+  postPatch = ''
+    chmod +x ./meson/post_install.py
+    patchShebangs ./meson/post_install.py
+  '';
 
   meta = {
-    description = "Extensible System Settings app Pantheon";
+    description = "Extensible System Settings app for Pantheon";
   };
 }
