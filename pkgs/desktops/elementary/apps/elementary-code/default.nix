bd4f7d3925e93656da577a1ea98d357207061773
worldofpeace <worldofpeace@users.noreply.github.com>
2018-08-15 22:46:19 -0400

    elementary-code: 2018-06-25 -> 2018-08-12

--- pkgs/desktops/elementary/apps/elementary-code/default.nix
+++ pkgs/desktops/elementary/apps/elementary-code/default.nix
@@ -1,17 +1,16 @@
 { mkElementary, lib, pkgconfig, meson, ninja, vala, substituteAll
-, desktop-file-utils, gettext, libxml2, glib, gtk3
-, granite, libgee, gsettings-desktop-schemas, gobjectIntrospection
-, defaultIconTheme, appstream, editorconfig-core-c, gtksourceview3
-, gtkspell3, libpeas, libsoup, vte, webkitgtk, zeitgeist, ctags
-, libgit2-glib, intltool, wrapGAppsHook }:
+, desktop-file-utils, gettext, libxml2, glib, gtk3, granite, libgee
+, gsettings-desktop-schemas, gobjectIntrospection, defaultIconTheme
+, appstream, editorconfig-core-c, gtksourceview3, gtkspell3, libpeas
+, libsoup, vte, webkitgtk, zeitgeist, ctags, libgit2-glib, intltool, wrapGAppsHook }:
 
 mkElementary rec {
   pname = "code";
-  version = "6ea55ad137df4b81c5fae0a8c745bb0cd110d505";
+  version = "959b5715b1e5c08fc3ab61d8ba7d65bf991052b2";
 
-  name = "elementary-${pname}-2018-06-25";
-  
-  sha256 = "0vha25mp0xhb1m1927h920nhnalicgrkz8qqfi5fl1d2c915wvxg";
+  name = "elementary-${pname}-2018-08-12";
+
+  sha256 = "1jdaqvsg6v5wvnkbxfa6nva97k0ajxx5nrvkb9fli17zsb9g0g6w";
 
   nativeBuildInputs = [
     appstream
@@ -45,7 +44,7 @@ mkElementary rec {
     webkitgtk
     zeitgeist
   ];
-  
+
   patches = [
     (substituteAll {
       src = ./ctags.patch;
