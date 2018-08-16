2428e6aded47a26700309cca9f9a34120fb7a60e
worldofpeace <worldofpeace@users.noreply.github.com>
2018-08-15 23:15:46 -0400

    elementary-gtk-theme: 5.1.1 -> 2018-08-15

--- pkgs/desktops/elementary/themeing/elementary-gtk-theme/default.nix
+++ pkgs/desktops/elementary/themeing/elementary-gtk-theme/default.nix
@@ -1,19 +1,17 @@
-{ mkElementary }:
+{ mkElementary, meson, ninja }:
 
 mkElementary rec {
   pname = "stylesheet";
-  version = "5.1.1";
+  version = "24e5a70adfefc293e7bd19a33090f05a03cc05fe";
 
-  name = "elementary-gtk-theme-${version}";
+  name = "elementary-gtk-theme-2018-08-15";
 
-  sha256 = "1749byc2lbxmprladn9n7k6jh79r8ffgayjn689gmqsrm6czsmh2";
+  sha256 = "0hdm4h2pd7gjps9g9yhjzgv7slcfdnrgprs0s2ww0l2sl9x8pbym";
 
-  dontBuild = true;
-
-  installPhase = ''
-    mkdir -p $out/share/themes/elementary
-    cp -r gtk-* plank $out/share/themes/elementary
-  '';
+  nativeBuildInputs = [
+    meson
+    ninja
+  ];
 
   meta = {
     description = "GTK theme designed to be smooth, attractive, fast, and usable";
