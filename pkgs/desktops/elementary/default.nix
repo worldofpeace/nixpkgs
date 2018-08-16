8f15141af77f895c5da18428c79888d7a57edf96
worldofpeace <worldofpeace@users.noreply.github.com>
2018-08-16 13:57:38 -0400

    elementary: cleanups at default.nix

--- pkgs/desktops/elementary/default.nix
+++ pkgs/desktops/elementary/default.nix
@@ -3,35 +3,27 @@
 
 lib.makeScope pkgs.newScope (self: with self; {
   vala = pkgs.vala_0_40;
-
-  mkElementary = callPackage ./mkElementary.nix {};
+  libgee = pkgs.gnome3.libgee;
+  libgda = pkgs.gnome3.libgda;
+  libpeas = pkgs.gnome3.libpeas;
 
   defaultIconTheme = elementary-icon-theme;
 
-  elementary-terminal = callPackage ./apps/elementary-terminal {
-    inherit (gnome3) vte libgee;
-  };
+  mkElementary = callPackage ./mkElementary.nix { };
 
-  elementary-calculator = callPackage ./apps/elementary-calculator {
-    inherit (gnome3) libgee;
-  };
+  elementary-calculator = callPackage ./apps/elementary-calculator { };
 
   elementary-camera = callPackage ./apps/elementary-camera { };
-
-  elementary-screenshot-tool = callPackage ./apps/elementary-screenshot-tool {
-    inherit (gnome3) libgee;
-  };
-
-  elementary-music = callPackage ./apps/elementary-music {
-    inherit (gnome3) libgee libgda libpeas;
-  };
-
-  elementary-videos = callPackage ./apps/elementary-videos {
-    inherit (gnome3) libgee;
-  };
 
   elementary-gtk-theme = callPackage ./elementary-gtk-theme { };
 
   elementary-icon-theme = callPackage ./elementary-icon-theme { };
 
+  elementary-music = callPackage ./apps/elementary-music { };
+
+  elementary-screenshot-tool = callPackage ./apps/elementary-screenshot-tool { };
+
+  elementary-terminal = callPackage ./apps/elementary-terminal { };
+
+  elementary-videos = callPackage ./apps/elementary-videos { };
 })
