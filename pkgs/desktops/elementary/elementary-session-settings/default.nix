56ad841c10bab9cd46b754c0b4579d15dcf99685
worldofpeace <worldofpeace@users.noreply.github.com>
2018-09-10 23:19:13 -0400

    elementary-session-settings: patch upstream autostarts with sed
    We can more easily accomplish patching g-s-d's autostarts and etc.
    by copying them and replacing the OnlyShowIn`s with sed instead.

--- pkgs/desktops/elementary/elementary-session-settings/default.nix
+++ pkgs/desktops/elementary/elementary-session-settings/default.nix
@@ -1,4 +1,4 @@
-{ mkElementary, substituteAll, gnome-settings-daemon, gnome-keyring, orca, xdg-user-dirs, gnome-session, wingpanel }:
+{ mkElementary, gnome-settings-daemon, gnome-keyring, gnome-session, wingpanel, orca }:
 
 mkElementary rec {
   pname = "session-settings";
@@ -10,22 +10,14 @@ mkElementary rec {
 
   dontBuild = true;
 
-  patches = [
-    (substituteAll {
-      src = ./autostart-exec.patch;
-      settings = "${gnome-settings-daemon}/libexec";
-      keyring = "${gnome-keyring}/bin";
-      orcaexec = "${orca}/bin";
-      dirs = "${xdg-user-dirs}/bin/";
-    })
-  ];
-
   installPhase = ''
-    mkdir -p $out/share/pantheon
+    mkdir -p $out/share
     cp -avr applications $out/share/
 
     mkdir -p $out/etc/xdg/autostart
-    cp -av autostart/* $out/etc/xdg/autostart
+    cp -av ${gnome-settings-daemon}/etc/xdg/autostart/* $out/etc/xdg/autostart
+    cp -av ${gnome-keyring}/etc/xdg/autostart/* $out/etc/xdg/autostart
+    cp -av ${orca}/etc/xdg/autostart/* $out/etc/xdg/autostart
 
     mkdir -p $out/share/gnome-session/sessions
     cp -av gnome-session/pantheon.session $out/share/gnome-session/sessions
@@ -38,6 +30,17 @@ mkElementary rec {
     substituteInPlace $out/share/xsessions/pantheon.desktop \
       --replace "gnome-session" "${gnome-session}/bin/gnome-session" \
       --replace "wingpanel" "${wingpanel}/bin/wingpanel"
+
+    for f in $out/etc/xdg/autostart/*; do mv "$f" "''${f%.desktop}-pantheon.desktop"; done
+
+    for autostart in $(grep -rl "OnlyShowIn=GNOME;" $out/etc/xdg/autostart)
+    do
+      echo "Patching OnlyShowIn to Pantheon in: $autostart"
+      sed -i "s,OnlyShowIn=GNOME;,OnlyShowIn=Pantheon;," $autostart
+    done
+
+    # Causes problems with lightdm and because I can't get GLOBIGNORE to work.
+    rm $out/etc/xdg/autostart/org.gnome.SettingsDaemon.ScreensaverProxy-pantheon.desktop
   '';
 
   meta = {
