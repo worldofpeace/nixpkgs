9b03af853b78e58c11ca5f7e3b2cf532c3ee8cc3
worldofpeace <worldofpeace@users.noreply.github.com>
2018-09-10 20:14:39 -0400

    elementary-screenshot-tool: 0.1.5 -> 2018-09-09

--- pkgs/desktops/elementary/apps/elementary-screenshot-tool/default.nix
+++ pkgs/desktops/elementary/apps/elementary-screenshot-tool/default.nix
@@ -4,11 +4,11 @@
 
 mkElementary rec {
   pname = "screenshot-tool";
-  version = "0.1.5";
+  version = "775b4d69fd52e75df0d58500728591e633f72f6d";
 
-  name = "elementary-${pname}-${version}";
+  name = "elementary-${pname}-2018-09-09";
 
-  sha256 = "1c9ixip25147pm30szqv7zd0xn5n9pdks6gxjxfi8dr67qzpflbd";
+  sha256 = "0wriml8drzx0ink58n7fmv0wnxvk62yp1n0gdz1bn7r6x7axfxh0";
 
   nativeBuildInputs = [
     desktop-file-utils
