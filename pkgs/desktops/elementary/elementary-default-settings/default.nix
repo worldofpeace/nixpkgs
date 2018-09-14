328384961b8f92c79b586d69b8d1680a567e157e
worldofpeace <worldofpeace@users.noreply.github.com>
2018-09-14 03:06:49 -0400

    elementary-default-settings: drop plank patching
    This still doesn't work for some reason

--- pkgs/desktops/elementary/elementary-default-settings/default.nix
+++ pkgs/desktops/elementary/elementary-default-settings/default.nix
@@ -10,17 +10,6 @@ mkElementary rec {
   sha256 = "13gcp342vz9w0pr9w268b4k6506z0wm7mrbb7q39zqrpl1x26xs7";
 
   patches = [
-    (substituteAll {
-      src = ./plank.patch;
-      gala = "${gala}";
-      elementary_calendar = "${elementary-calendar}";
-      elementary_music = "${elementary-music}";
-      elementary_photos = "${elementary-photos}";
-      switchboard_settings = "${switchboard}";
-      elementary_videos = "${elementary-videos}";
-      epiphany = "${epiphany}";
-      geary = "${geary}";
-    })
     ./background.patch
     ./wingpanel-overrides.patch
   ];
@@ -32,9 +21,6 @@ mkElementary rec {
     cp -av settings.ini $out/etc/gtk-3.0
 
     cp -av debian/elementary-default-settings.gsettings-override $out
-
-    mkdir -p $out/etc/skel/.config/plank
-    cp -avr plank/dock1 $out/etc/skel/.config/plank
 
     cp -avr wingpanel.d/ $out/etc/
   '';
