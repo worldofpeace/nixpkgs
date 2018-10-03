442a285f987e759210f0c48e891a1fea4b833fcf
worldofpeace <worldofpeace@users.noreply.github.com>
2018-10-03 14:44:53 -0400

    wingpanel-with-indicators: set SWITCHBOARD_PLUGS_PATH
    Needed to search settings within wingpanel-applications-menu

--- pkgs/desktops/elementary/wingpanel/wrapper.nix
+++ pkgs/desktops/elementary/wingpanel/wrapper.nix
@@ -1,4 +1,4 @@
-{ stdenv, makeWrapper, symlinkJoin, wingpanel, wingpanelIndicators, indicators ? null }:
+{ stdenv, makeWrapper, symlinkJoin, wingpanel, wingpanelIndicators, switchboard-with-plugs, indicators ? null }:
 
 let
   selectedIndicators = if indicators == null then wingpanelIndicators else indicators;
@@ -12,7 +12,8 @@ symlinkJoin {
 
   postBuild = ''
     wrapProgram $out/bin/wingpanel \
-      --set WINGPANEL_INDICATORS_PATH "$out/lib/wingpanel"
+      --set WINGPANEL_INDICATORS_PATH "$out/lib/wingpanel" \
+      --set SWITCHBOARD_PLUGS_PATH "${switchboard-with-plugs}/lib/switchboard" # wingpanel-applications-menu has a plugin to search switchboard settings
   '';
 
 }
