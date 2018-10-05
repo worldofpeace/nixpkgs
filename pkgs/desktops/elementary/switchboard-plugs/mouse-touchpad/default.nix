416f7b519030d0094d97fc60200c6868897e5194
worldofpeace <worldofpeace@users.noreply.github.com>
2018-10-05 14:43:23 -0400

    switchboard-plug-mouse-touchpad: 0.1.3 -> 2018-09-19

--- pkgs/desktops/elementary/switchboard-plugs/mouse-touchpad/default.nix
+++ pkgs/desktops/elementary/switchboard-plugs/mouse-touchpad/default.nix
@@ -3,15 +3,15 @@
 
 stdenv.mkDerivation rec {
   pname = "switchboard-plug-mouse-touchpad";
-  version = "0.1.3";
+  version = "84eb7078b118fe513b276ab5365a8f4242004d86";
 
-  name = "${pname}-${version}";
+  name = "${pname}-2018-09-19";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "1d91simqd4likx3lg3fi9bbiajkq4f95i3446nzqriy33m5g3yb2";
+    sha256 = "007dg0klr0v7fjiq658kybwdc2w10yygizlq6wcjh1df3y6kbris";
   };
 
   passthru = {
@@ -33,14 +33,6 @@ stdenv.mkDerivation rec {
     gtk3
     libgee
     switchboard
-  ];
-
-  # Fixes https://github.com/elementary/switchboard-plug-mouse-touchpad/issues/36
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/elementary/switchboard-plug-mouse-touchpad/commit/c54e7468cf65dba4d0e31cb8bcf182063d9d924e.patch";
-      sha256 = "1izm3c69ibgn65f55irlf6jkrzym61yzhr75z5nqi9hw5jbcyncy";
-    })
   ];
 
   PKG_CONFIG_SWITCHBOARD_2_0_PLUGSDIR = "lib/switchboard";
