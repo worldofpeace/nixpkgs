906c62c51289be87c1a69bbf282bebbd6028204e
worldofpeace <worldofpeace@users.noreply.github.com>
2018-10-16 16:56:53 -0400

    elementary-gtk-theme: 2018-10-04 -> 5.2.0

--- pkgs/desktops/elementary/themeing/elementary-gtk-theme/default.nix
+++ pkgs/desktops/elementary/themeing/elementary-gtk-theme/default.nix
@@ -2,15 +2,15 @@
 
 stdenv.mkDerivation rec {
   pname = "stylesheet";
-  version = "d8be6eb36531e347e777f412f6f0d8af05553371";
+  version = "5.2.0";
 
-  name = "elementary-gtk-theme-2018-10-04";
+  name = "elementary-gtk-theme-${version}";
 
   src = fetchFromGitHub {
     owner = "elementary";
     repo = pname;
     rev = version;
-    sha256 = "106lsayy6gcwfnv7ixxm7lgzwyi57by0sa37rzw9naa8fr7a2ki5";
+    sha256 = "0rfpqkbrrbaga8mdpr5s48f13w634dfyk4l44bmg8nxhvzcd88m4";
   };
 
   passthru = {
