41aa6144ebd191e647e891dc2a084202cd323e37
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 19:24:29 +0100

    gnome3.gnome-disk-utility: 3.30.2 -> 3.31.90

--- pkgs/desktops/gnome-3/core/gnome-disk-utility/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-disk-utility/default.nix
@@ -5,11 +5,11 @@
 
 stdenv.mkDerivation rec {
   name = "gnome-disk-utility-${version}";
-  version = "3.30.2";
+  version = "3.31.90";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gnome-disk-utility/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "1365fabz3q7n3bl775z82m1nzg18birxxyd7l2ssbbkqrx3h7wgi";
+    sha256 = "0i1bhgnj14y7gd87wshyzg4qczkpr5hamd2qwr5kw72vq3cgrscx";
   };
 
   nativeBuildInputs = [
