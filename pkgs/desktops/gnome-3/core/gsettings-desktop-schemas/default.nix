182e2138fa7ed5950fda0b0813fb87a28116c872
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-03 18:23:57 +0100

    gsettings-desktop-schemas: 3.28.1 -> 3.31.91

--- pkgs/desktops/gnome-3/core/gsettings-desktop-schemas/default.nix
+++ pkgs/desktops/gnome-3/core/gsettings-desktop-schemas/default.nix
@@ -1,23 +1,29 @@
 { stdenv, fetchurl, pkgconfig, intltool, glib, gobject-introspection
+, meson
+, ninja
+, python3
   # just for passthru
 , gnome3 }:
 
 stdenv.mkDerivation rec {
   name = "gsettings-desktop-schemas-${version}";
-  version = "3.28.1";
+  version = "3.31.91";
 
   src = fetchurl {
     url = "mirror://gnome/sources/gsettings-desktop-schemas/${stdenv.lib.versions.majorMinor version}/${name}.tar.xz";
-    sha256 = "0bshwm49cd01ighsxqlbqn10q0ch71ff99gcrx8pr2gyky2ad3pq";
+    sha256 = "1flhb48knlm69mk09dy4aa9md54zw2p3qaa0qx37wkqxz2j048fs";
   };
 
   passthru = {
     updateScript = gnome3.updateScript { packageName = "gsettings-desktop-schemas"; };
   };
 
+  # meson installs the schemas to share/glib-2.0/schemas
+  # We add the override file there too so it will be compiled and later moved by
+  # glib's setup hook.
   preInstall = ''
-    mkdir -p $out/share/gsettings-schemas/${name}/glib-2.0/schemas
-    cat - > $out/share/gsettings-schemas/${name}/glib-2.0/schemas/remove-backgrounds.gschema.override <<- EOF
+    mkdir -p $out/share/glib-2.0/schemas
+    cat - > $out/share/glib-2.0/schemas/remove-backgrounds.gschema.override <<- EOF
       [org.gnome.desktop.background]
       picture-uri='''
 
@@ -26,9 +32,14 @@ stdenv.mkDerivation rec {
     EOF
   '';
 
+  postPatch = ''
+    chmod +x build-aux/meson/post-install.py
+    patchShebangs build-aux/meson/post-install.py
+  '';
+
   buildInputs = [ glib gobject-introspection ];
 
-  nativeBuildInputs = [ pkgconfig intltool ];
+  nativeBuildInputs = [ pkgconfig python3 meson ninja ];
 
   meta = with stdenv.lib; {
     maintainers = gnome3.maintainers;
