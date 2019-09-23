947c7e2c91e5d5739458fd7adb5d87800900f5cd
worldofpeace <worldofpeace@protonmail.ch>
2019-09-23 18:50:22 -0400

    gnomeExtensions.clipboard-indicator: mark as broken

--- pkgs/desktops/gnome-3/extensions/clipboard-indicator/default.nix
+++ pkgs/desktops/gnome-3/extensions/clipboard-indicator/default.nix
@@ -24,5 +24,6 @@ stdenv.mkDerivation rec {
     maintainers = with maintainers; [ jonafato ];
     platforms = platforms.linux;
     homepage = https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator;
+    broken = versionAtLeast gnome3.gnome-shell.version "3.26";
   };
 }
