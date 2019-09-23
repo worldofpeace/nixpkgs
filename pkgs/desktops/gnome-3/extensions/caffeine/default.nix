8fcb3df81d062e52c2eed434a760b62236d10d3e
worldofpeace <worldofpeace@protonmail.ch>
2019-09-23 18:50:03 -0400

    gnomeExtensions.caffeine: make as broken

--- pkgs/desktops/gnome-3/extensions/caffeine/default.nix
+++ pkgs/desktops/gnome-3/extensions/caffeine/default.nix
@@ -32,5 +32,6 @@ stdenv.mkDerivation rec {
     license = licenses.gpl2;
     maintainers = with maintainers; [ eperuffo ];
     homepage = https://github.com/eonpatapon/gnome-shell-extension-caffeine;
+    broken = versionAtLeast gnome3.gnome-shell.version "3.32"; # Doesn't support 3.34
   };
 }
